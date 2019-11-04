9f6cc96dce0c6bbae8a450f775aa28647b1bbccc
dtpiffero <57331925+dtpiffero@users.noreply.github.com>
2019-11-04 14:47:36 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsd</code> 
+          Edit <code>Jack is the big gay</code> 
         </p>
         <a
           className="App-link"
